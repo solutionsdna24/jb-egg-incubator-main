c3c707c258f915a8907ac59fb21724360cf821ec
Dipak R <solutionsdna24@gmail.com>
2025-07-26 23:57:47 +0530

    Configured for domain url

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/jb-egg-incubator-main">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
